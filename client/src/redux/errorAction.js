9371fe8d9bd0f880429c17ef975ba09d73169d2f
Satyam <satyam83soni@gmail.com>
2024-08-13 02:33:38 +0530

    update api

--- client/src/redux/errorAction.js
+++ client/src/redux/errorAction.js
@@ -1,17 +1,20 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const FETCH_ERRORS_REQUEST = 'FETCH_ERRORS_REQUEST';
-export const FETCH_ERRORS_SUCCESS = 'FETCH_ERRORS_SUCCESS';
-export const FETCH_ERRORS_FAILURE = 'FETCH_ERRORS_FAILURE';
-export const RESOLVE_ERROR_SUCCESS = 'RESOLVE_ERROR_SUCCESS';
+export const FETCH_ERRORS_REQUEST = "FETCH_ERRORS_REQUEST";
+export const FETCH_ERRORS_SUCCESS = "FETCH_ERRORS_SUCCESS";
+export const FETCH_ERRORS_FAILURE = "FETCH_ERRORS_FAILURE";
+export const RESOLVE_ERROR_SUCCESS = "RESOLVE_ERROR_SUCCESS";
 
 export const fetchErrors = (page) => {
   return async (dispatch) => {
     dispatch({ type: FETCH_ERRORS_REQUEST });
     try {
-      const response = await axios.get('http://3.110.110.198:3000//error/getErrors', {
-        params: { q: false, page },
-      });
+      const response = await axios.get(
+        "http://3.110.110.198:3000/error/getErrors",
+        {
+          params: { q: false, page },
+        }
+      );
       dispatch({ type: FETCH_ERRORS_SUCCESS, payload: response.data });
     } catch (error) {
       dispatch({ type: FETCH_ERRORS_FAILURE, payload: error.message });
@@ -22,7 +25,7 @@ export const fetchErrors = (page) => {
 export const resolveError = (id) => {
   return async (dispatch) => {
     try {
-      const response = await axios.patch(`http://3.110.110.198:3000//error/${id}`);
+      const response = await axios.patch(`http://3.110.110.198:3000/error/${id}`);
       if (response.status === 200) {
         dispatch({ type: RESOLVE_ERROR_SUCCESS, payload: id });
       }
